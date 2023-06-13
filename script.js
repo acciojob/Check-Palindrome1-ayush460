d306dbdc3515fdbb46a035983561efd96b27a44b
ayush460 <90319070+ayush460@users.noreply.github.com>
2023-06-13 22:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
-function palindrome(str){
+function isPalindrome(s) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
+  // Check if the string is equal to its reverse
+  return s === s.split('').reverse().join('');
 }
-module.exports = palindrome
+
+// Test the function
+console.log(isPalindrome("race a car")); // Output: false
+console.log(isPalindrome("racecar")); // Output: true
+console.log(isPalindrome("RaceCar")); // Output: true
+console.log(isPalindrome("raceCAR")); // Output: true
